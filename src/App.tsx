fee6c574fb084b13967444231d53ced150324d79
johnsmccain <johnsdanlami@gmail.com>
2023-01-10 14:29:47 +0100

    type savety

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
 import { useState } from 'react';
 import domtoimage from "dom-to-image";
 import './app.scss';
+import one from "./assets/one.jpg";
+import two from "./assets/two.jpg";
+import three from "./assets/three.jpg";
+import four from "./assets/four.jpg";
+ 
 
 function App() {
 
@@ -14,23 +19,23 @@ function App() {
     setLoading(true)
     let temp: string[] = []
   
-    let i = 4;
+    let i:number = 4;
     while (i){
       let res = await fetch("https://source.unsplash.com/random/300x300");
       temp.push(res.url);
-      console.log(res.url)
+      // console.log(res.url)
       i--
     }
     setPhotos(temp)
+    // setPhotos([one, two, three, four])
     setLoading(false)
   }
 
-console.log(loading);
   const savePhoto = async () => {
     const preview:any = document.querySelector(".preview")
     domtoimage.toJpeg(preview, { quality: 0.95 })
     .then(function (dataUrl) {
-      var a = document.createElement('a');
+      var a: HTMLAnchorElement = document.createElement('a');
       a.download = `${name}-${Date.now()}`;
       a.href = dataUrl;
       a.click();
@@ -55,37 +60,62 @@ console.log(loading);
                 </div>
               : photos.length > 0 ? 
                 <div className='images'>
-                  {photos.map((img, i) => <img key={i} onClick={() => setPhoto(img)} src={img} alt="random"/>)}
+                  {photos.map((img, i) => (
+                    <img key={i} onClick={() => setPhoto(img)} src={img} alt="random"/>)
+                  )}
                 </div>
               : 
                 <>
                   <p className='text init'>
                     {
-                      loading ?
-                        "Loading..."
-                      :
-                        "Oops, click to generate new photos"
+                      loading ? "Loading..."
+                      : "Oops, click to generate new photos"
                     }
                   </p>
                   <button 
                     onClick={generatePhotos}
                     disabled={loading}
                   >
-                      {!loading ? "Generate" : "Please Wait"}
+                    {
+                      !loading ? "Generate" : "Please Wait"
+                    }
                   </button>
                 </>
             } 
         </div>
-        { photo && <div className="row">
-          <button onClick={generatePhotos} className="col reset btn">Reset</button>
-          <input type="text" className="caption col" placeholder='Name here...' onChange={(e)=> setName(e.target.value)}/>
-          <button onClick={savePhoto}  className='download btn col'>
-            download
-          </button>
-        </div>}
+        { photo && 
+          <div className="row">
+            <button 
+              onClick={generatePhotos} 
+              className="col reset btn"
+            >
+              Reset
+            </button>
+            <input 
+              type="text" 
+              className="caption col" 
+              maxLength={12}
+              placeholder='Name here...' 
+              onChange={(e)=> setName(e.target.value)}
+            />
+            <button 
+              onClick={savePhoto} 
+              className='download btn col'
+            >
+              download
+            </button>
+          </div>
+        }
     </div>
     <footer>
-      <a className='footer link' href="https://github.com/johnsmccain/ASSETSMENT" target="_blank" rel="noopener noreferrer">Source Code</a>
+      <a 
+        className='footer link' 
+        href="https://github.com/johnsmccain/ASSETSMENT" 
+        target="_blank" 
+        rel="noopener noreferrer"
+      >
+        Source Code
+      </a>
     </footer>
   </div>
 )}
